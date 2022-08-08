1c0208467dded600ca377948fc52aae026981dbe
Erkam <erkam.bozan@gmail.com>
2022-08-08 03:30:50 +0300

    fix: changing default port for heroku

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
     "log"
     "net/http"
-
+    "os"
+    "fmt"
 )
 
 func main() {
@@ -11,8 +12,12 @@ func main() {
 
     app.Initialize()
 
-    port, err := os.Getenv("PORT")
+    port := os.Getenv("PORT")
 
+    if port == "" {
+        port = "3000"
+    }
+    fmt.Printf("Port : %s", port)
     log.Fatal(http.ListenAndServe(":"+port, app.Router))
 
 }
